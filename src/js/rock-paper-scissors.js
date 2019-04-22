b0aa8560750f5a4f67e6dff4316b185bab638bb1
zanenkn <zane.neikena@gmail.com>
2019-04-22 17:03:11 +0200

    adds a function that randomly selects between rock, paper and scissors (computers choice)

--- src/js/rock-paper-scissors.js
+++ src/js/rock-paper-scissors.js
@@ -5,7 +5,20 @@ function RockPaperScissors () {
     
 }
 
+//Users choice by clicking button
 
 function SetUserChoice(choice) {
     UserChoice = choice
 }
+
+// Computers choice (random)
+function SetComputersChoice() {
+    let num = Math.floor((Math.random() * 3) + 1)
+    if (num==1) {
+        ComputersChoice="rock"
+    } else if (num==2) {
+        ComputersChoice="paper"
+    } else {ComputersChoice="scissors"}
+}
+
+SetComputersChoice();
