91e9c88c98f6222f03ce74649e58a206c11a2e2f
zanenkn <zane.neikena@gmail.com>
2019-04-26 20:51:37 +0200

    Extracts the determining winner function to dom.js and re-writes the Computers choice function in order to use Sinon stubs for tests

---
+++ src/js/dom.js
@@ -0,0 +1,24 @@
+// Determining the winner
+document.addEventListener('DOMContentLoaded', () => {
+    let choice = document.querySelectorAll("button[tag='choice']")
+    for(var i = 0; i < choice.length; i++){
+
+        choice[i].addEventListener("click", function() {
+            event.preventDefault();
+            let computersChoice = Computer.choice();
+
+            if (userChoice == computersChoice) {
+                document.getElementById("message").innerHTML = `You chose ${userChoice}, computer chose ${computersChoice}. It is a tie.`
+            } else if (
+                (userChoice == "rock" && computersChoice == "scissors") ||
+                (userChoice == "scissors" && computersChoice == "paper") ||
+                (userChoice == "paper" && computersChoice == "rock")
+            ) {
+                document.getElementById("message").innerHTML = `You chose ${userChoice}, computer chose ${computersChoice}. You win!`
+            } else {
+                document.getElementById("message").innerHTML = `You chose ${userChoice}, computer chose ${computersChoice}. You lose!`
+            }    
+        })    
+    }
+
+})
--- src/js/rock-paper-scissors.js
+++ src/js/rock-paper-scissors.js
@@ -1,43 +1,45 @@
 //Users choice by clicking button
-
-function SetUserChoice(choice) {
-    UserChoice = choice
+function setUserChoice(choice) {
+    userChoice = choice
 }
 
 // Computers choice (random)
-function SetComputersChoice() {
-    let num = Math.floor((Math.random() * 3) + 1)
-    if (num==1) {
-        ComputersChoice="rock"
-    } else if (num==2) {
-        ComputersChoice="paper"
-    } else {ComputersChoice="scissors"}
-    
-    return ComputersChoice
+const Computer = {
+    choice: function () {
+        let num = Math.floor((Math.random() * 3) + 1)
+        if (num==1) {
+            return "rock"
+        } else if (num==2) {
+            return "paper"
+        } else {return "scissors"}
+    }
 }
 
 
-// Determining the winner
-document.addEventListener('DOMContentLoaded', () => {
-    let choice = document.querySelectorAll("button[tag='choice']")
-    for(var i = 0; i < choice.length; i++){
+// // function setComputersChoice() {
+// //     let num = Math.floor((Math.random() * 3) + 1)
+// //     if (num==1) {
+// //         computersChoice="rock"
+// //     } else if (num==2) {
+// //         computersChoice="paper"
+// //     } else {computersChoice="scissors"}
+    
+// //     return computersChoice
+// // }
 
-        choice[i].addEventListener("click", function() {
-            event.preventDefault();
-            SetComputersChoice();
 
-            if (UserChoice == ComputersChoice) {
-                document.getElementById("message").innerHTML = `You chose ${UserChoice}, computer chose ${ComputersChoice}. It is a tie.`
-            } else if (
-                (UserChoice == "rock" && ComputersChoice == "scissors") ||
-                (UserChoice == "scissors" && ComputersChoice == "paper") ||
-                (UserChoice == "paper" && ComputersChoice == "rock")
-            ) {
-                document.getElementById("message").innerHTML = `You chose ${UserChoice}, computer chose ${ComputersChoice}. You win!`
-            } else {
-                document.getElementById("message").innerHTML = `You chose ${UserChoice}, computer chose ${ComputersChoice}. You lose!`
-            }    
-        })    
-    }
 
-})
+
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = setUserChoice;
+}
+
+// if (typeof module !== 'undefined' && module.exports) {
+//     module.exports = setComputersChoice;
+// }
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Computer;
+}
+
