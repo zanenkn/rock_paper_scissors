025cccc4b58af60c9290c538aec0ecea910153d5
zanenkn <zane.neikena@gmail.com>
2019-05-02 23:08:07 +0200

    Adds a new unit test for computers choice

--- spec/rock-paper-scissors.spec.js
+++ spec/rock-paper-scissors.spec.js
@@ -1,38 +1,16 @@
 require('../spec.helper')
-var sinon = require('sinon');
 
-describe ("Stub", () => {
-  afterEach(() => {
-    sinon.restore();
+
+describe ("Rock Paper Scissors", () => {
+  it('computer has a random choice', () => {
+    let choice = setComputersChoice();
+    expect(choice).to.be.oneOf(["rock","paper","scissors"]) 
   });
 
-  it("stub1 works", () => {
-    let comp = new Computer
-    sinon.stub(comp, 'choice').returns("rock")
-    
-    expect(comp.choice).to.have.returned("rock")
-    // expect(comp.choice()).to.eql("rock")
-  });
 
-  // it("stub2 works", () => {
-  //   sinon.stub(Computer, 'choice').get(function getterFn() {
-  //     return 'rock';
-  //   });
+})
 
-  //   expect(Computer.choice).to.eql("rock")
-  // });
 
-  // it("stub3 works", () => {
-  //   sinon.stub(Computer, 'choice').value('rock')
-  //   expect(Computer.choice).to.eql("rock")
-  // });
 
-  // it ("user chooses 'rock' and game is a tie", () => {
-  //   let comp = new Computer
-  //   sinon.stub(comp, 'choice').returns("rock")
-  //   expect(msg).to.eql("tie")
 
-  // });
-
-});
 
