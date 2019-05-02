e8029bb9f266b86b0f19ca681c5e4d3a55c29cb5
zanenkn <zane.neikena@gmail.com>
2019-05-02 22:01:12 +0200

    Adds feature tests for app returning a message after Player clicks on a button.

--- features/application.feature.js
+++ features/application.feature.js
@@ -5,7 +5,6 @@ context('The game', () => {
   before(async () => {
     await browser.init()
     await browser.visitPage('http://localhost:8080/')  
-    await sinon.stub(Math, 'floor').returns(1);
   });
 
   beforeEach(async () => {
@@ -16,24 +15,22 @@ context('The game', () => {
     browser.close();
   });
 
-  it("returns 'tie' message if user picks 'rock' and computer picks 'rock'", async () => {
-    
+  it("returns a message, containing 'You chose rock' after player clicks 'rock' button", async () => {
     await browser.clickOnButton("button[id='rock']");
     let content = await browser.getContent("div[id='message']");
-    expect(content).to.contain('You chose rock, computer chose rock. It is a tie.');
+    expect(content).to.contain('You chose rock');
+  });
+
+  it("returns a message, containing 'You chose paper' after player clicks 'paper' button", async () => {
+    await browser.clickOnButton("button[id='paper']");
+    let content = await browser.getContent("div[id='message']");
+    expect(content).to.contain('You chose paper');
+  });
+
+  it("returns a message, containing 'You chose scissors' after player clicks 'scissors' button", async () => {
+    await browser.clickOnButton("button[id='scissors']");
+    let content = await browser.getContent("div[id='message']");
+    expect(content).to.contain('You chose scissors');
   });
 
 });
-
-
-// stub1
-// let computer = new Computer
-// await sinon.stub(computer, 'choice').returns("rock")
-
-// stub2
-// sinon.stub(Computer, 'choice').get(function getterFn() {
-//   return 'rock';
-// });
-
-// stub3
-// sinon.stub(Computer, 'choice').value('rock')
