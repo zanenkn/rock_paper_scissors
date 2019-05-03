c8c3c60b8a9909370e40958e9b1e92fb489491fc
zanenkn <zane.neikena@gmail.com>
2019-05-03 09:16:33 +0200

    Adds the function setComputersChoice to the spec file

--- spec/rock-paper-scissors.spec.js
+++ spec/rock-paper-scissors.spec.js
@@ -1,5 +1,15 @@
 require('../spec.helper')
 
+function setComputersChoice() {
+  let num = Math.floor((Math.random() * 3) + 1)
+  if (num==1) {
+      computersChoice="rock"
+  } else if (num==2) {
+      computersChoice="paper"
+  } else {computersChoice="scissors"}
+
+  return computersChoice
+}
 
 describe ("Rock Paper Scissors", () => {
   it('computer has a random choice', () => {
